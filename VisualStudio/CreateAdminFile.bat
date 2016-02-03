d9be6cbb60067f7c97d1dda629f1fb48ba49a0a3
Shawn Morrison <smorrison1986@gmail.com>
2016-02-02 23:18:05 -0600

    Improving CreateAdminFile script.

--- VisualStudio/CreateAdminFile.bat
+++ VisualStudio/CreateAdminFile.bat
@@ -1,2 +1,7 @@
-vs_community_ENU.exe /CreateAdminFile %~dp0\..\AdminDeployment.xml
+vs_community_ENU.exe /CreateAdminFile %~dp0\AdminDeployment.xml /quiet
+if %ERRORLEVEL% NEQ 0 (
+	@echo ERROR: Failed to create AdminDeployment.xml file!
+) else (
+	@echo AdminDeployment.xml creation succeeded!
+)
 pause
